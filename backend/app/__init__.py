a47d1afb53d1fe065313e52b5c0bc2928b2acaa7
4ndres-292 <choquecahuanaandresoriginal@gmail.com>
2025-06-08 00:03:52 -0400

    intent

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,15 @@
+# En tu archivo app/__init__.py o similar
 from flask import Flask
-from .core.database import iniciar_base_datos
-from .routes.auth_routes import auth_bp
-from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
-    iniciar_base_datos(app)
-    app.register_blueprint(auth_bp)
+    app.config.from_object("config")  # asegúrate de tener la DB URL aquí
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # ⚠️ Crea todas las tablas declaradas con SQLAlchemy
+
     return app
