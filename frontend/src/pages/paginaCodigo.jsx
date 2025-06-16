15dd76fac9d408554beba4f2ade6db5ceaf1f1ca
4ndres-292 <choquecahuanaandresoriginal@gmail.com>
2025-06-16 09:10:45 -0400

    primer intent docker

--- frontend/src/pages/paginaCodigo.jsx
+++ frontend/src/pages/paginaCodigo.jsx
@@ -4,42 +4,53 @@ import BotonCompilar from '../components/botonCompilar'; // ajusta la ruta segú
 import React, { useState } from "react";
 import EntradaCodigo from "../components/entradaCodigo";
 import ArchivoCompilador from "../components/archivoCompilador";
+import BarraFunciones from "../components/barraFunciones";
 
 const PaginaCodigo = () => {
   const [resultado, setResultado] = useState('');
   const [codigo, setCodigo] = useState('');
 
   const manejarCompilacion = () => {
-    console.log(codigo);
+    // console.log(codigo);
+    fetch(`/ejecutar`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ codigo }) 
+    })
+      .then(response => response.json())
+      .then(data => {
+        setResultado(data.mensaje);
+      })
+      .catch(error => {
+        setResultado("Error al conectar con el backend: " + error.message);
+      });
 
-    // fetch("http://127.0.0.1:5000/compilar", 
-    //   method 
-    // )
-
-    const errores = `
-  Error: línea 12: 'x' no está definido
-  Error: línea 18: se esperaba ';'
-  Error: línea 25: tipo de datos incompatible
-      `;
-    setResultado(errores);
+  //   const errores = `
+  // Error: línea 12: 'x' no está definido
+  // Error: línea 18: se esperaba ';'
+  // Error: línea 25: tipo de datos incompatible
+  //     `;
+    // setResultado(errores);
   };
   return (
-    <div className="p-6 text-white">
-      <div className="flex flex-col items-center w-full">
+    <div className="p-2 text-white">
+      <div className="flex flex-col items-center w-full pb-2">
         <h1 className="text-3xl font-bold mb-2 underline text-center">Tarea 1</h1>
-        <p className="mb-2 pl-10 text-white text-xl text-left italic w-full">Hacer un programa que te devuelva ¡Hola Mundo!</p>
+        <p className="mb-4 pl-10 text-white text-xl text-left italic w-full">Hacer un programa que te devuelva ¡Hola Mundo!</p>
       </div>
-      <div className="flex justify-end">
-        <BotonCompilar onCompilar={manejarCompilacion} /></div>
-      <div className="grid grid-cols-[1fr_2fr] gap-4">
-        <div className="w-full">
+      <div className="grid grid-cols-[300px_1fr_300px] gap-2">
+        <div className="row-span-3">
           <ArchivoCompilador></ArchivoCompilador>
         </div>
-        <div className="w-full">
+        <div className="flex flex-col gap-3">
+          <BarraFunciones onCompilar={manejarCompilacion} ></BarraFunciones>
           <EntradaCodigo onChangeCode={setCodigo}></EntradaCodigo>
-          <div className="flex justify-end">
-          </div>
           <ResultadoCompilacion resultado={resultado} />
+        </div>
+        <div className="row-span-3">
+          <input type="text" placeholder="Ingresa caracteres aquí" className="p-2 border border-gray-400 rounded" />
         </div>
       </div>
     </div>
